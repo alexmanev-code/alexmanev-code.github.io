29fbb0ba11bc51171adbb51656d833b0b1f743ed
Alex Manev <mane0003@hz.nl>
2024-09-05 23:12:00 +0200

    Modified pages layouts/ added js folder

---
+++ js/index.js
@@ -0,0 +1,29 @@
+
+function toggleSidebar() {
+    const sidebar = document.getElementById('sidebar');
+    const sidebarContent = document.getElementById('sidebar-content');
+    
+    
+    sidebar.classList.toggle('collapsed');
+    
+    if (sidebar.classList.contains('collapsed')) {
+        sidebarContent.classList.add('collapsed');
+    } else {
+        sidebarContent.classList.remove('collapsed');
+    }
+}
+
+function showParagraph(paraNumber) {
+    document.getElementById('para1').classList.add('hidden');
+    document.getElementById('para2').classList.add('hidden');
+    
+    if (paraNumber === 1) {
+        document.getElementById('para1').classList.remove('hidden');
+    } else if (paraNumber === 2) {
+        document.getElementById('para2').classList.remove('hidden');
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    showParagraph(1);});
+    
