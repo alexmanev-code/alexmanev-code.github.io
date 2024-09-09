9b37642805feadbf40dc5496558ba86319e4dd6c
Alex Manev <mane0003@hz.nl>
2024-09-09 21:57:34 +0200

    Dashboard initial implementation/index tweaked

--- js/index.js
+++ js/index.js
@@ -16,9 +16,11 @@ function toggleSidebar() {
 function showParagraph(paraNumber) {
     document.getElementById('para1').classList.add('hidden');
     document.getElementById('para2').classList.add('hidden');
+    document.getElementById('list').classList.add('hidden');
     
     if (paraNumber === 1) {
         document.getElementById('para1').classList.remove('hidden');
+        document.getElementById('list').classList.remove('hidden');
     } else if (paraNumber === 2) {
         document.getElementById('para2').classList.remove('hidden');
     }
